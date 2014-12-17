f41919286119dc8c7b2d874d1dbecb8a133d2fcd
jdmcpeek <david.mcpeek@yale.edu>
2014-12-16 23:39:17 -0500

    going to have one massive scope object

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,30 +20,39 @@ angular.module('emilyChristmas2014App')
       ],
       menuOpen: false
     };
-    $scope.bodies = [
-      { image: 'images/santa_body.png', onCanvas: true },
-      { image: 'images/elf_body.png', onCanvas: false },
-      { image: 'images/octocat.png', onCanvas: false },
-      { image: 'images/bodybuilder.png', onCanvas: false },
-      { image: 'images/jessica_rabbit.png', onCanvas: false },
-      { image: 'images/pookie_bear.png', onCanvas: false },
-      { image: 'images/gorilla.png', onCanvas: false }
-    ];
-    $scope.hats = [
-      { image: 'images/santa_hat.png', onCanvas: true },
-      { image: 'images/elf_hat.png', onCanvas: false },
-      { image: 'images/bowler.png', onCanvas: false },
-      { image: 'images/top.png', onCanvas: false },
-      { image: 'images/propeller.png', onCanvas: false },
-      { image: 'images/giraffe.png', onCanvas: false }
-    ];
-    $scope.backgrounds = [
-      { image: 'images/north_pole.png', onCanvas: true },
-      { image: 'images/santa_workshop.png', onCanvas: false },
-      { image: 'images/tropics.png', onCanvas: false },
-      { image: 'images/jungle.png', onCanvas: false },
-      { image: 'images/woodlands.png', onCanvas: false },
-      { image: 'images/mordor.png', onCanvas: false },
-      { image: 'images/casablanca.png', onCanvas: false }
-    ];
+    $scope.bodies = {
+      images: [ 
+        { image: 'images/santa_body.png', onCanvas: true },
+        { image: 'images/elf_body.png', onCanvas: false },
+        { image: 'images/octocat.png', onCanvas: false },
+        { image: 'images/bodybuilder.png', onCanvas: false },
+        { image: 'images/jessica_rabbit.png', onCanvas: false },
+        { image: 'images/pookie_bear.png', onCanvas: false },
+        { image: 'images/gorilla.png', onCanvas: false }
+      ],
+      menuOpen: false
+    };
+    $scope.hats = {
+      images: [ 
+        { image: 'images/santa_hat.png', onCanvas: true },
+        { image: 'images/elf_hat.png', onCanvas: false },
+        { image: 'images/bowler.png', onCanvas: false },
+        { image: 'images/top.png', onCanvas: false },
+        { image: 'images/propeller.png', onCanvas: false },
+        { image: 'images/giraffe.png', onCanvas: false }
+      ],
+      menuOpen: false
+    };
+    $scope.backgrounds = {
+      images: [ 
+        { image: 'images/north_pole.png', onCanvas: true },
+        { image: 'images/santa_workshop.png', onCanvas: false },
+        { image: 'images/tropics.png', onCanvas: false },
+        { image: 'images/jungle.png', onCanvas: false },
+        { image: 'images/woodlands.png', onCanvas: false },
+        { image: 'images/mordor.png', onCanvas: false },
+        { image: 'images/casablanca.png', onCanvas: false }
+      ],
+      menuOpen: false
+    };
   });
