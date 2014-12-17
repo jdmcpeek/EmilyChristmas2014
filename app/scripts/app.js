881c513c3dfa3c9d96ee347c0e1aa430160be0d9
jdmcpeek <david.mcpeek@yale.edu>
2014-12-16 19:55:44 -0500

    app.js specifies routes. When I changed the name it screwed up routing and pointed to a nonexistent controller. Fixed now.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,7 +21,7 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'selectionsController'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
