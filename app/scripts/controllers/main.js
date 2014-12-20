21f6944f9faa3c299de1f5a15daf5c29d4bb816c
jdmcpeek <david.mcpeek@yale.edu>
2014-12-20 14:02:10 -0800

    Next step: photoshop all images to correct dimensions"

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,46 +10,38 @@
 
 angular.module('emilyChristmas2014App')
   .controller('emilyChristmasController', function ($scope) {
-    $scope.faces = {
-      images: [
-        { image: 'images/emily_pic1.png', onCanvas: true },
-        { image: 'images/emily_pic2.png', onCanvas: true },
-        { image: 'images/emily_pic3.png', onCanvas: true },
-        { image: 'images/emily_pic4.png', onCanvas: true },
-        { image: 'images/emily_pic5.png', onCanvas: true },
-        { image: 'images/emily_pic6.png', onCanvas: true }
-      ]
-    };
-    $scope.bodies = {
-      images: [
-        { image: 'images/santa_body.png', onCanvas: true },
-        { image: 'images/elf_body.png', onCanvas: true },
-        { image: 'images/octocat.png', onCanvas: true },
-        { image: 'images/bodybuilder.png', onCanvas: true },
-        { image: 'images/jessica_rabbit.png', onCanvas: true },
-        { image: 'images/pookie_bear.png', onCanvas: true },
-        { image: 'images/gorilla.png', onCanvas: true }
-      ]
-    };
-    $scope.hats = {
-      images: [
-        { image: 'images/santa_hat.png', onCanvas: true },
-        { image: 'images/elf_hat.png', onCanvas: true },
-        { image: 'images/bowler.png', onCanvas: true },
-        { image: 'images/top.png', onCanvas: true },
-        { image: 'images/propeller.png', onCanvas: true },
-        { image: 'images/giraffe.png', onCanvas: true }
-      ]
-    };
-    $scope.backgrounds = {
-      images: [
-        { image: 'images/north_pole.png', onCanvas: true },
-        { image: 'images/claymation_christmas.png', onCanvas: true },
-        { image: 'images/tropics.jpg', onCanvas: true },
-        { image: 'images/mordor.jpg', onCanvas: true },
-        { image: 'images/casablanca.jpg', onCanvas: true }
-      ]
-    };
+    $scope.faces = [
+        'images/emily_pic1.png',
+        'images/emily_pic2.png',
+        'images/emily_pic3.png',
+        'images/emily_pic4.png',
+        'images/emily_pic5.png',
+        'images/emily_pic6.png'
+      ];
+    $scope.bodies = [
+        'images/santa_body.png',
+        'images/elf_body.png',
+        'images/octocat.png',
+        'images/bodybuilder.png',
+        'images/jessica_rabbit.png',
+        'images/pookie_bear.png',
+        'images/gorilla.png',
+      ];
+    $scope.hats = [
+        'images/santa_hat.png',
+        'images/elf_hat.png',
+        'images/bowler.png',
+        'images/top.png',
+        'images/propeller.png',
+        'images/giraffe.png',
+      ];
+    $scope.backgrounds = [
+        'images/north_pole.png',
+        'images/claymation_christmas.png',
+        'images/tropics.jpg',
+        'images/mordor.jpg',
+        'images/casablanca.jpg',
+      ];
     $scope.menu = 'init';
     $scope.face = 'images/emily_pic1.png';
     $scope.hat = 'images/santa_hat.png';
