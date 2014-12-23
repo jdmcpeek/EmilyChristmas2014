d0780bd535021ee49c628996e89edc328ffb238b
jdmcpeek <david.mcpeek@yale.edu>
2014-12-23 00:41:23 -0800

    Nuked the routing and adding selections buttons to top

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,17 +16,4 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]);
